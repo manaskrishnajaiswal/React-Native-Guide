019c828c6a608f7684760b5bf591fa861ed773b3
manas5996 <manas5996@gmail.com>
2022-11-09 17:58:53 +0530

    After Flexbox- a deep dive

--- RNCourse_1/App.js
+++ RNCourse_1/App.js
@@ -2,15 +2,59 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+    <>
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Your course goal!" />
+          <Button title="Add Goal" />
+        </View>
+        <View>
+          <Text>List of Goals...</Text>
+        </View>
       </View>
-      <View>
-        <Text>List of Goals...</Text>
+      <View
+        style={{
+          padding: 50,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "red",
+            width: 100,
+            height: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "blue",
+            width: 100,
+            height: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "green",
+            width: 100,
+            height: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
