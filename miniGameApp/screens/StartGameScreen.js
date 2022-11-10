3f9d2f917b084438d25b7ad0373c18828e856b2f
manas5996 <manas5996@gmail.com>
2022-11-10 13:40:55 +0530

    After setting up our screen components

---
+++ miniGameApp/screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TextInput, Button, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <Text>Buttons</Text>
+    </View>
+  );
+};
+
+export default StartGameScreen;
