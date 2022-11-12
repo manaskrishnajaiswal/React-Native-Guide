22ada05bc684030b4a36ae3221a2152d87e7b1e1
manas5996 <manas5996@gmail.com>
2022-11-12 08:51:19 +0530

    After starting work on the gamescreen

--- miniGameApp/screens/GameScreen.js
+++ miniGameApp/screens/GameScreen.js
@@ -2,7 +2,24 @@ import React from "react";
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>{/* Log Rounds */}</View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
