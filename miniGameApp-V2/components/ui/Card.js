ad5c91f7cc2634ab12105d3ba7b75b162a214647
manas5996 <manas5996@gmail.com>
2022-11-14 09:05:37 +0530

    After introducing the dimensions API

--- miniGameApp-V2/components/ui/Card.js
+++ miniGameApp-V2/components/ui/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 const Card = ({ children }) => {
@@ -8,11 +8,14 @@ const Card = ({ children }) => {
 
 export default Card;
 
+const deviceWidth = Dimensions.get("window").width;
+// console.log(deviceWidth);
+
 const styles = StyleSheet.create({
   card: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
