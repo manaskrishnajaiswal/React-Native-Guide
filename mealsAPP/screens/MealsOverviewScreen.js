a197e864ad91d3467a68085acb5e4c6cfb3d6e8e
manas5996 <manas5996@gmail.com>
2022-11-15 21:14:16 +0530

    After displaying meals

--- mealsAPP/screens/MealsOverviewScreen.js
+++ mealsAPP/screens/MealsOverviewScreen.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 const MealsOverviewScreen = ({ route }) => {
   const catID = route.params.categoryID;
-  //   console.log(catID);
+
+  const displayedMEals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catID) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
-    <View>
-      <Text>Meals Overview Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMEals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
@@ -15,7 +28,7 @@ const MealsOverviewScreen = ({ route }) => {
 export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
-  constainer: {
+  container: {
     flex: 1,
     padding: 16,
   },
