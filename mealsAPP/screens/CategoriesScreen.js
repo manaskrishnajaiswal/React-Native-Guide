62c6309e13a3a5b6b7ee36af074164b5cb1c0de7
manas5996 <manas5996@gmail.com>
2022-11-15 15:39:12 +0530

    After getting started with the app and outputting meal categories

---
+++ mealsAPP/screens/CategoriesScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTitle from "../components/CategoryGridTitle";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+}
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+};
+
+export default CategoriesScreen;
