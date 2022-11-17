43223750968be9713c69b9f40c7c221b1cf9fe60
manas5996 <manas5996@gmail.com>
2022-11-17 13:35:28 +0530

    After adding a drawer navigation and creating a drawer

--- other-navigators-starting-project/App.js
+++ other-navigators-starting-project/App.js
@@ -1,5 +1,21 @@
-import { Text } from 'react-native';
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Welcome">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({});
